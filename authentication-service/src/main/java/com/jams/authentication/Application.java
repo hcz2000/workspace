45abdd187483c9a264124f7a5fe6cdaa4190b3df
hcz2000 <huangchangzhan@hotmail.com>
2018-09-10 14:58:12 +0800

    update

--- authentication-service/src/main/java/com/jams/authentication/Application.java
+++ authentication-service/src/main/java/com/jams/authentication/Application.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class Application {
     @RequestMapping(value = { "/user" }, produces = "application/json")
     public Map<String, Object> user(OAuth2Authentication user) {
-        Map<String, Object> userInfo = new HashMap();
+        Map<String, Object> userInfo = new HashMap<String, Object>();
         userInfo.put("user", user.getUserAuthentication().getPrincipal());
         userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
         return userInfo;
--- authentication-service/src/main/java/com/jams/authentication/security/WebSecurityConfigurer.java
+++ authentication-service/src/main/java/com/jams/authentication/security/WebSecurityConfigurer.java
@@ -7,28 +7,23 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-
 @Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-        @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-   @Override
-    @Bean
-    public UserDetailsService userDetailsServiceBean() throws Exception {
-        return super.userDetailsServiceBean();
-    }
+	@Override
+	@Bean
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		return super.userDetailsServiceBean();
+	}
 
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("john.carnell").password("password1").roles("USER")
-                .and()
-                .withUser("william.woodward").password("password2").roles("USER", "ADMIN");
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("john.carnell").password("password1").roles("USER").and()
+				.withUser("william.woodward").password("password2").roles("USER", "ADMIN");
+	}
 }

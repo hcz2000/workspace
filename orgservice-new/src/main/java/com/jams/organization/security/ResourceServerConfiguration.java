25008fe13b00a4be2e0122da9c66dddcb4f496c2
hcz2000 <huangchangzhan@hotmail.com>
2018-09-10 21:56:00 +0800

    update

--- orgservice-new/src/main/java/com/jams/organization/security/ResourceServerConfiguration.java
+++ orgservice-new/src/main/java/com/jams/organization/security/ResourceServerConfiguration.java
@@ -1,5 +1,6 @@
 package com.jams.organization.security;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -7,7 +8,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/v1/organizations/**").hasRole("ADMIN").anyRequest().authenticated();
 	}
 	
 

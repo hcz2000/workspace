82861069c7498a943c9b2f955264854263fa1707
hcz2000 <huangchangzhan@hotmail.com>
2018-09-22 14:34:47 +0800

    update

--- orgservice-new/src/main/java/com/jams/organization/utils/UserContextFilter.java
+++ orgservice-new/src/main/java/com/jams/organization/utils/UserContextFilter.java
@@ -20,7 +20,7 @@ public class UserContextFilter implements Filter {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
 
-        System.out.println("****** I am entering the licensing service id with auth token: " + httpServletRequest.getHeader("Authorization"));
+        System.out.println("****** I am entering the organization service id with auth token: " + httpServletRequest.getHeader("Authorization"));
 
 
         UserContextHolder.getContext().setCorrelationId(  httpServletRequest.getHeader(UserContext.CORRELATION_ID) );

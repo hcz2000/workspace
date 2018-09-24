19df1bf07143aa94eeb56b62a5b4b3e883e41f2a
hcz2000 <huangchangzhan@hotmail.com>
2018-09-24 22:39:53 +0800

    init

--- mySpark/src/main/java/com/jams/spark/WordCount.java
+++ mySpark/src/main/java/com/jams/spark/WordCount.java
@@ -1,7 +1,6 @@
 package com.jams.spark;
 
 import java.util.Arrays;  
-
 import org.apache.spark.SparkConf;  
 import org.apache.spark.api.java.JavaPairRDD;  
 import org.apache.spark.api.java.JavaRDD;  

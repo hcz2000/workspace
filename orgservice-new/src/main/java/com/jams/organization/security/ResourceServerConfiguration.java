60ff1064ce76df8dbbe66f166699a38e03b7c1af
hcz2000 <huangchangzhan@hotmail.com>
2018-10-06 20:12:35 +0800

    update

--- orgservice-new/src/main/java/com/jams/organization/security/ResourceServerConfiguration.java
+++ orgservice-new/src/main/java/com/jams/organization/security/ResourceServerConfiguration.java
@@ -1,9 +1,11 @@
 package com.jams.organization.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Configuration
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
 	@Override

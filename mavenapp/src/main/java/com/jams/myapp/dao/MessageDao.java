06467a087cce5f872fd601140b36c4a837fedb0c
hcz2000 <huangchangzhan@hotmail.com>
2018-10-07 09:38:24 +0800

    delete

--- mavenapp/src/main/java/com/jams/myapp/dao/MessageDao.java
+++
@@ -1,9 +0,0 @@
-package com.jams.myapp.dao;
-
-import java.util.List;
-
-import com.jams.myapp.entity.Message;
-
-public interface MessageDao {  
-    public List<Message> findMyMessage(String receiver); 
-}  

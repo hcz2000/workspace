bff37aa4e5c2d294c9987f327d23f666252c7a67
hcz2000 <huangchangzhan@hotmail.com>
2018-10-19 17:54:45 +0800

    update

--- portalsvr/src/main/java/com/jams/portal/Application.java
+++ portalsvr/src/main/java/com/jams/portal/Application.java
@@ -2,15 +2,15 @@ package com.jams.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
-//@EnableEurekaServer
-//@EnableConfigServer
+@EnableEurekaServer
+@EnableConfigServer
 @EnableAuthorizationServer
 public class Application {
 	public static void main(String[] args) {

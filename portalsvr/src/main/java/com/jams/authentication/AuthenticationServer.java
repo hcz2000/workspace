1e4893fe646f90aebb81c80674667567f309923a
hcz2000 <huangchangzhan@hotmail.com>
2018-10-20 11:57:40 +0800

    update

---
+++ portalsvr/src/main/java/com/jams/authentication/AuthenticationServer.java
@@ -0,0 +1,16 @@
+package com.jams.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableAuthorizationServer
+public class AuthenticationServer {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServer.class,args);
+	}
+
+}

fd0206472792f631c1ccbfd0d0048bc70290e15c
hcz2000 <huangchangzhan@hotmail.com>
2018-10-21 22:02:30 +0800

    update

--- portalsvr/src/main/java/com/jams/authentication/security/JWTOAuth2Config.java
+++ portalsvr/src/main/java/com/jams/authentication/security/JWTOAuth2Config.java
@@ -4,10 +4,10 @@ package com.jams.authentication.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -22,7 +22,7 @@ public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private JPAUserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;//inited by WebSecurityConfigurer
     
     //ClientDetailsService case
     @Autowired
@@ -30,10 +30,8 @@ public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private TokenStore tokenStore;
-
-    @Autowired
-    private DefaultTokenServices tokenServices;
-
+    
+ 
     @Autowired
     private JwtAccessTokenConverter jwtAccessTokenConverter;
 

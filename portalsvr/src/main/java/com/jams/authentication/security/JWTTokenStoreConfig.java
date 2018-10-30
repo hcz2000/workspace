13f57394228e85c815b7026d33697480fb1c29cc
hcz2000 <huangchangzhan@hotmail.com>
2018-10-30 21:47:55 +0800

    update

--- portalsvr/src/main/java/com/jams/authentication/security/JWTTokenStoreConfig.java
+++ portalsvr/src/main/java/com/jams/authentication/security/JWTTokenStoreConfig.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 
 import com.jams.authentication.config.ServiceConfig;
 
@@ -34,6 +36,20 @@ public class JWTTokenStoreConfig {
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter converter=new JwtAccessTokenConverter();
+		/*
+	    KeyStoreKeyFactory keyStoreKeyFactory =new KeyStoreKeyFactory(new ClassPathResource("config/mystore.jks"), "mypass".toCharArray());
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mykey"));
+        
+        Resource resource = new ClassPathResource("config/publickey.txt");
+        String publicKey = null;
+        try {
+                publicKey = inputStream2String(resource.getInputStream());
+        } catch (final IOException e) {
+                throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
+
+		*/
 		converter.setSigningKey(serviceConfig.getJwtSigningKey());
 		return converter;
 	}

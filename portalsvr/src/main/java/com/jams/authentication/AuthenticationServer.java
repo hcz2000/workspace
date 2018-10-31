e4212b3514dfc4424ec0954ed2a537ac4f93f615
hcz2000 <huangchangzhan@hotmail.com>
2018-10-31 11:12:48 +0800

    update

--- portalsvr/src/main/java/com/jams/authentication/AuthenticationServer.java
+++ portalsvr/src/main/java/com/jams/authentication/AuthenticationServer.java
@@ -2,14 +2,12 @@ package com.jams.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableAuthorizationServer
-//@EnableConfigServer
 public class AuthenticationServer {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServer.class,args);

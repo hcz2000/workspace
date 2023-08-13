92efc574626ffb64991f24e3267b4a72432ada18
hcz <huangchangzhan@hotmail.com>
2023-08-13 22:36:50 +0800

    update

--- HttpClient/src/com/zhan/myreader/webapi/BookApi.java
+++ HttpClient/src/com/zhan/myreader/webapi/BookApi.java
@@ -20,168 +20,189 @@ import java.util.Map;
 
 public class BookApi {
 
-    public static void getNewChapterCount(Book book, final ResultCallback callback){
-        String[] urls=book.getChapterUrl().split("\\|");
-        String indexUrl=urls[0];
+	public static void getNewChapterCount(Book book, final ResultCallback callback) {
+		String[] urls = book.getChapterUrl().split("\\|");
+		String indexUrl = urls[0];
 
-        if(BookSource.tianlai.toString().equals(book.getSource())) {
-        	HttpUtil.httpGet_Async(indexUrl, null, "GBK", new ResultCallback() {
-                @Override
-                public void onFinish(Object o, int code) {
-                	int maxChapterNo= TianlaiUtil.getMaxChapterNoFromHtml((String) o,book);
-                	callback.onFinish(maxChapterNo,0);
-                }
+		if (BookSource.tianlai.toString().equals(book.getSource())) {
+			HttpUtil.httpGet_Async(indexUrl, null, "GBK", new ResultCallback() {
+				@Override
+				public void onFinish(Object o, int code) {
+					int maxChapterNo = TianlaiUtil.getMaxChapterNoFromHtml((String) o, book);
+					callback.onFinish(maxChapterNo, 0);
+				}
 
-                @Override
-                public void onError(Exception e) {
-                    callback.onError(e);
-                }
-            });
-        }else {
-        	BookApi.getBookChapters(book, new ResultCallback() {
-                @Override
-                public void onFinish(Object o, int code) {
-                    final List<Chapter> chapters = (List<Chapter>) o;
-                    callback.onFinish(chapters.size(),0);
-                }
+				@Override
+				public void onError(Exception e) {
+					callback.onError(e);
+				}
+			});
+		} else {
+			BookApi.getBookChapters(book, new ResultCallback() {
+				@Override
+				public void onFinish(Object o, int code) {
+					final List<Chapter> chapters = (List<Chapter>) o;
+					callback.onFinish(chapters.size(), 0);
+				}
 
-                @Override
-                public void onError(Exception e) {
-                	callback.onError(e);
-                }
-            });
-        }
-        
-    }
-    
-    public static void getBookChapters(Book book, final ResultCallback callback){
-    	String[] urls=book.getChapterUrl().split("\\|");
-   		String indexUrl=urls[0];
-   		final int startPos;
-		if(urls.length>1)
-   			startPos=Integer.parseInt(urls[1]);
-   		else
-   			startPos=0;
-		HttpUtil.httpGet_Async(indexUrl, null, "utf-8",	new ResultCallback() {
+				@Override
+				public void onError(Exception e) {
+					callback.onError(e);
+				}
+			});
+		}
+
+	}
+
+	public static void getBookChapters(Book book, final ResultCallback callback) {
+		String[] urls = book.getChapterUrl().split("\\|");
+		String indexUrl = urls[0];
+		final int startPos;
+		if (urls.length > 1)
+			startPos = Integer.parseInt(urls[1]);
+		else
+			startPos = 0;
+		HttpUtil.httpGet_Async(indexUrl, null, "utf-8", new ResultCallback() {
 			@Override
 			public void onFinish(Object html, int code) {
-				List<Chapter> list= TianlaiUtil.getChaptersFromHtml((String) html,book);
-   	            if(startPos>0) {
-					for(Chapter chapter: list) {
-						chapter.setNumber(chapter.getNumber()+startPos);
-					}
-   	            }
-   	            callback.onFinish(list,0);
-   	            if(!indexUrl.equals(book.getChapterUrl().split("\\|")[0])) {
-   	            	String newChapterUrl=book.getChapterUrl().split("\\|")[0]+"|"+(startPos+list.size());
-   	            	book.setChapterUrl(newChapterUrl);
-   	            	getBookChapters(book,callback);
-   	            }
+				List<Chapter> list = TianlaiUtil.getChaptersFromHtml((String) html, book, startPos);
+				callback.onFinish(list, 0);
+				if (!indexUrl.equals(book.getChapterUrl().split("\\|")[0])) {
+					getBookChapters(book, callback);
+				}
 			}
 
 			@Override
 			public void onError(Exception e) {
-   	            callback.onError(e);
-   	        }
+				callback.onError(e);
+			}
 		});
-     }
+	}
 
+	public static List<Chapter> getBookChapters(Book book) {
+		List<Chapter> list = new ArrayList<Chapter>();
+		String[] urls = book.getChapterUrl().split("\\|");
+		String indexUrl = urls[0];
+		int startPos;
+		if (urls.length > 1)
+			startPos = Integer.parseInt(urls[1]);
+		else
+			startPos = 0;
+        for(;;) {
+     String pageHtml = HttpUtil.httpGet_Sync(indexUrl);
+    		if (pageHtml == null)
+    			 break;
+    		else {
+    			List<Chapter> volume=TianlaiUtil.getChaptersFromHtml((String) pageHtml, book, startPos);
+    			list.addAll(volume);
+    			startPos+=volume.size();
+    			String newUrl=book.getChapterUrl().split("\\|")[0];
+    			    if(indexUrl.equals(newUrl))
+    			    	 break;
+    			    else
+    			  indexUrl=newUrl;
+    	}
+   }
+		return list;
+	}
 
-    public static void getChapterContent(Chapter chapter, final ResultCallback callback){
-		String url=chapter.getUrl();
+	public static void getChapterContent(Chapter chapter, final ResultCallback callback) {
+		String url = chapter.getUrl();
 
 		HttpUtil.httpGet_Async(url, null, "utf-8", new ResultCallback() {
-            @Override
-            public void onFinish(Object html, int code) {
-            	final StringBuffer content=new StringBuffer();
-            	content.append(TianlaiUtil.getContentFromHtml((String)html));
-            	if(content.toString().equals("")) {
-            		callback.onFinish("", 0);
-            		return;
-            	}
-            		
-            	String nextPage= TianlaiUtil.getNextPageFromHtml((String)html);
-            	if(nextPage!=null) {
+			@Override
+			public void onFinish(Object html, int code) {
+				final StringBuffer content = new StringBuffer();
+				content.append(TianlaiUtil.getContentFromHtml((String) html));
+				if (content.toString().equals("")) {
+					callback.onFinish("", 0);
+					return;
+				}
+
+				String nextPage = TianlaiUtil.getNextPageFromHtml((String) html);
+				if (nextPage != null) {
 					HttpUtil.httpGet_Async(nextPage, null, "utf-8", new ResultCallback() {
-                        public void onFinish(Object html, int code) {
-                        	String page2= TianlaiUtil.getContentFromHtml((String)html);
-                        	//start with"\0xa1 \0xa1"
-                        	if(page2.length()>2)
-                        		content.append(page2.substring(2));
-                        	
-                        	String nextPage= TianlaiUtil.getNextPageFromHtml((String)html);
-                        	if(nextPage!=null) {
+						public void onFinish(Object html, int code) {
+							String page2 = TianlaiUtil.getContentFromHtml((String) html);
+							// start with"\0xa1 \0xa1"
+							if (page2.length() > 2)
+								content.append(page2.substring(2));
+
+							String nextPage = TianlaiUtil.getNextPageFromHtml((String) html);
+							if (nextPage != null) {
 								HttpUtil.httpGet_Async(nextPage, null, "utf-8", new ResultCallback() {
 									public void onFinish(Object html, int code) {
-										String page3= TianlaiUtil.getContentFromHtml((String)html);
-										if(page3.length()>2)
+										String page3 = TianlaiUtil.getContentFromHtml((String) html);
+										if (page3.length() > 2)
 											content.append(page3.substring(2));
 										callback.onFinish(content.toString(), 0);
 									}
-									public void onError(Exception e) {  
-										callback.onError(e); 
-									}
-                        		});
-                        	}else
-                        		callback.onFinish(content.toString(),0);
-                        }
-                        public void onError(Exception e) { 
-                        	callback.onError(e); 
-                        }
-                    });
-            	}else
-                	callback.onFinish(content.toString(),0);
-            }
-            public void onError(Exception e) {
-                callback.onError(e);
-            }
-        });
-    }
 
-	public static String getChapterContent(Chapter chapter){
-		StringBuilder content=new StringBuilder();
+									public void onError(Exception e) {
+										callback.onError(e);
+									}
+								});
+							} else
+								callback.onFinish(content.toString(), 0);
+						}
+
+						public void onError(Exception e) {
+							callback.onError(e);
+						}
+					});
+				} else
+					callback.onFinish(content.toString(), 0);
+			}
+
+			public void onError(Exception e) {
+				callback.onError(e);
+			}
+		});
+	}
+
+	public static String getChapterContent(Chapter chapter) {
+		StringBuilder content = new StringBuilder();
 		String page1Url = chapter.getUrl();
-		String page1Html=HttpUtil.httpGet_Sync(page1Url);
-		if(page1Html==null)
+		String page1Html = HttpUtil.httpGet_Sync(page1Url);
+		if (page1Html == null)
 			return null;
 		content.append(TianlaiUtil.getContentFromHtml(page1Html));
-		if(content.toString().equals("")) {
+		if (content.toString().equals("")) {
 			chapter.setContent("");
 		}
 
-		String page2Url= TianlaiUtil.getNextPageFromHtml(page1Html);
-		if(page2Url!=null){
-			String page2Html=HttpUtil.httpGet_Sync(page2Url);
-			if(page2Html==null)
+		String page2Url = TianlaiUtil.getNextPageFromHtml(page1Html);
+		if (page2Url != null) {
+			String page2Html = HttpUtil.httpGet_Sync(page2Url);
+			if (page2Html == null)
 				return null;
-			String page2= TianlaiUtil.getContentFromHtml(page2Html);
-			String page3Url= TianlaiUtil.getNextPageFromHtml(page2Html);
-			if(page2.length()>2) {
+			String page2 = TianlaiUtil.getContentFromHtml(page2Html);
+			String page3Url = TianlaiUtil.getNextPageFromHtml(page2Html);
+			if (page2.length() > 2) {
 				content.append(page2.substring(2));
 			}
-			if(page3Url!=null){
-				String page3Html=HttpUtil.httpGet_Sync(page3Url);
-				if(page3Html==null)
+			if (page3Url != null) {
+				String page3Html = HttpUtil.httpGet_Sync(page3Url);
+				if (page3Html == null)
 					return null;
-				String page3= TianlaiUtil.getContentFromHtml(page3Html);
-				if(page3.length()>2)
+				String page3 = TianlaiUtil.getContentFromHtml(page3Html);
+				if (page3.length() > 2)
 					content.append(page3.substring(2));
 			}
 		}
 		return content.toString();
 	}
 
-
-    public static void search(String key, final ResultCallback callback){
-		if(StringHelper.isNumeric(key)){
-			String bookUrl=URLCONST.bookprefix_tianlai+"/"+key.substring(0,key.length()-3)+"/"+key+"/";
+	public static void search(String key, final ResultCallback callback) {
+		if (StringHelper.isNumeric(key)) {
+			String bookUrl = URLCONST.bookprefix_tianlai + "/" + key.substring(0, key.length() - 3) + "/" + key + "/";
 			System.out.println(bookUrl);
 			HttpUtil.httpGet_Async(bookUrl, null, "utf-8", new ResultCallback() {
 				@Override
 				public void onFinish(Object html, int code) {
-					ArrayList<Book> result=new ArrayList();
-					Book book=TianlaiUtil.getBookFromHtml(html.toString());
-					if(book!=null)
+					ArrayList<Book> result = new ArrayList();
+					Book book = TianlaiUtil.getBookFromHtml(html.toString());
+					if (book != null)
 						result.add(book);
 					callback.onFinish(result, code);
 				}
@@ -191,7 +212,7 @@ public class BookApi {
 					callback.onError(e);
 				}
 			});
-		}else {
+		} else {
 			Map<String, Object> params = new HashMap<>();
 			params.put("keyword", key);
 			HttpUtil.httpGet_Async(URLCONST.method_buxiu_search, params, "utf-8", new ResultCallback() {
@@ -206,7 +227,6 @@ public class BookApi {
 				}
 			});
 		}
-    }
-
+	}
 
 }

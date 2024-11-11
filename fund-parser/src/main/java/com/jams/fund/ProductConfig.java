aafa93a901e0c003e681a89a5fa472ea04ebf463
hcz <huangchangzhan@hotmail.com>
2024-11-11 18:45:22 +0800

    update

---
+++ fund-parser/src/main/java/com/jams/fund/ProductConfig.java
@@ -0,0 +1,52 @@
+package com.jams.fund;
+
+public class ProductConfig {
+	private String code;
+	private String desc;
+	private String url;
+	private String type;
+	
+	public ProductConfig() {
+		
+	}
+
+	public ProductConfig(String code, String desc, String url, String type) {
+		super();
+		this.code = code;
+		this.desc = desc;
+		this.url = url;
+		this.type = type;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}

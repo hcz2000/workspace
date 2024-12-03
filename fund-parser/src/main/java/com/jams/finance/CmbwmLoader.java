f88f29ea5bc4a64233f84e53693e977e542913ed
hcz <huangchangzhan@hotmail.com>
2024-12-03 14:54:41 +0800

    update

--- fund-parser/src/main/java/com/jams/finance/CmbwmLoader.java
+++ fund-parser/src/main/java/com/jams/finance/CmbwmLoader.java
@@ -1,28 +1,44 @@
 package com.jams.finance;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.htmlunit.options.HtmlUnitDriverOptions;
+import org.openqa.selenium.htmlunit.options.HtmlUnitOption;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.htmlunit.BrowserVersion;
 import org.htmlunit.html.HtmlElement;
 
 
-public class CmbwmLoader0 extends BaseLoader{
-	private FirefoxDriver driver;
+public class CmbwmLoader extends BaseLoader{
+	private WebDriver driver;
 	
-	public CmbwmLoader0() {
+	public CmbwmLoader() {
         super();
-        System.setProperty("webdriver.gecko.driver", "../../pywork/Tagui/geckodriver.exe");
+       
+        //System.setProperty("webdriver.gecko.driver", "../../pywork/Tagui/geckodriver.exe");
 		FirefoxOptions opts=new FirefoxOptions();
 		opts.setCapability("pageLoadStrategy", "none");
 		driver=new FirefoxDriver(opts);
+		
+        /*
+        HtmlUnitDriverOptions opts = new HtmlUnitDriverOptions(BrowserVersion.FIREFOX);;
+		opts.setCapability(HtmlUnitOption.optJavaScriptEnabled,true);
+		opts.setCapability(HtmlUnitOption.optPrintContentOnFailingStatusCode,true);
+		opts.setCapability(HtmlUnitOption.optThrowExceptionOnScriptError, false);
+		opts.setCapability(HtmlUnitOption.optCssEnabled,false);
+		driver=new HtmlUnitDriver(opts);
+		*/
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	}
@@ -33,12 +49,11 @@ public class CmbwmLoader0 extends BaseLoader{
         double last_net_value = lastRecord.getValue();
         System.out.println("##" + last_sync_date + "  " + last_net_value);
 		driver.get(url+"?prodTradeCode="+code+"&prodClcMode=01&finType=P");
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//div[@class='flex-sub']/div[2]/div[1]/span[1]"),"销售代码："+code));
 
 		WebElement contentDiv=driver.findElement(By.xpath("//div[@class='proTabList']"));
 		List<WebElement> menuList = contentDiv.findElements(By.xpath("//div[@class='item text-14 flex']"));
-		System.out.println(menuList.size());
 		for (WebElement menu : menuList) {
 			if ("产品净值".equals(menu.getText())) {
 				menu.click();
@@ -83,7 +98,7 @@ public class CmbwmLoader0 extends BaseLoader{
 	}
 
 	public static void main(String[] args) {
-		try(CmbwmLoader0 boc=new CmbwmLoader0()){
+		try(CmbwmLoader boc=new CmbwmLoader()){
 			boc.refreshCatalog();
 		}catch (Exception e) {
 			e.printStackTrace();

21e9567b331c90d916e1ca5e6d50ace523cd7738
hcz <huangchangzhan@hotmail.com>
2024-12-20 11:24:55 +0800

    update

--- fund-parser/src/main/java/com/jams/finance/Task.java
+++ fund-parser/src/main/java/com/jams/finance/Task.java
@@ -15,25 +15,33 @@ public class Task {
 	@Scheduled(cron="0 20 8,13 * * ?")
     public void process(){
 		try(BocwmLoader boc=new BocwmLoader()){
+			System.out.println("----处理中银理财 ----");
 			boc.refreshCatalog();
+			System.out.println("----中银理财处理完成!----");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		try (CgbwmLoader cgb = new CgbwmLoader()) {
+			System.out.println("----处理广银理财 ----");
 			cgb.refreshCatalog();
+			System.out.println("----广银理财处理完成!----");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		try(CmbwmLoader cmb=new CmbwmLoader()){
+			System.out.println("----处理招银理财 ----");
 			cmb.refreshCatalog();
+			System.out.println("----招银理财处理完成!----");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		try(PawmLoader pa=new PawmLoader()){
+			System.out.println("----处理平安理财 ----");
 			pa.refreshCatalog();
+			System.out.println("----平安理财处理完成!----");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
